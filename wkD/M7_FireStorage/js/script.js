c340a744d2bdf84e94a44455546b5b2cfaf2e48c
Chi-Shan Yu <chsyu@tea.ntue.edu.tw>
2016-11-10 22:48:34 +0800

    FireStorage

---
+++ wkD/M7_FireStorage/js/script.js
@@ -0,0 +1,201 @@
+$(document).ready(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBG2Rp9w8QOBKypmFFsR7ZjGfsU7l83VVY",
+    authDomain: "authchatwithpersonaldata.firebaseapp.com",
+    databaseURL: "https://authchatwithpersonaldata.firebaseio.com",
+    storageBucket: "authchatwithpersonaldata.appspot.com",
+    messagingSenderId: "253660279157"
+  };
+  firebase.initializeApp(config);
+
+  // Firebase database reference
+  var dbChatRoom = firebase.database().ref().child('chatroom');
+  var dbUser = firebase.database().ref().child('user');
+
+  var photoURL;
+
+  // REGISTER DOM ELEMENTS
+  const $messageField = $('#messageInput');
+  const $nameField = $('#nameInput');
+  const $messageList = $('#example-messages');
+  const $email = $('#email');
+  const $password = $('#password');
+  const $btnSignIn = $('#btnSignIn');
+  const $btnSignUp = $('#btnSignUp');
+  const $btnSignOut = $('#btnSignOut');
+  const $message = $('#example-messages');
+  const $hovershadow = $('.hover-shadow');
+  const $btnSubmit = $('#btnSubmit');
+  const $signInfo = $('#sign-info');
+  const $file = $('#file');
+
+
+  // Hovershadow
+  $hovershadow.hover(
+    function(){
+      $(this).addClass("mdl-shadow--4dp");
+    },
+    function(){
+      $(this).removeClass("mdl-shadow--4dp");
+    }
+  );
+
+  var storageRef = firebase.storage().ref();
+
+  function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    var file = evt.target.files[0];
+
+    var metadata = {
+      'contentType': file.type
+    };
+
+    // Push to child path.
+    // [START oncomplete]
+    storageRef.child('images/' + file.name).put(file, metadata).then(function(snapshot) {
+      console.log('Uploaded', snapshot.totalBytes, 'bytes.');
+      console.log(snapshot.metadata);
+      photoURL = snapshot.metadata.downloadURLs[0];
+      console.log('File available at', photoURL);
+    }).catch(function(error) {
+      // [START onfailure]
+      console.error('Upload failed:', error);
+      // [END onfailure]
+    });
+    // [END oncomplete]
+  }
+
+  window.onload = function() {
+    $file.change(handleFileSelect);
+    // $file.disabled = false;
+  }
+
+  // SignIn/SignUp/SignOut Button status
+  var user = firebase.auth().currentUser;
+  if (user) {
+    $btnSignIn.attr('disabled', 'disabled');
+    $btnSignUp.attr('disabled', 'disabled');
+    $btnSignOut.removeAttr('disabled')
+  } else {
+    $btnSignOut.attr('disabled', 'disabled');
+    $btnSignIn.removeAttr('disabled')
+    $btnSignUp.removeAttr('disabled')
+  }
+
+  // Sign In
+  $btnSignIn.click(function(e){
+    const email = $email.val();
+    const pass = $password.val();
+    const auth = firebase.auth();
+    // signIn
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(function(e){
+      console.log(e.message);
+      $signInfo.html(e.message);
+    });
+    promise.then(function(){
+      console.log('SignIn User');
+    });
+  });
+
+  // SignUp
+  $btnSignUp.click(function(e){
+    const email = $email.val();
+    const pass = $password.val();
+    const auth = firebase.auth();
+    // signUp
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(function(e){
+      console.log(e.message);
+      $signInfo.html(e.message);
+    });
+    promise.then(function(user){
+      console.log("SignUp user is "+user.email);
+      const dbUserid = dbUser.child(user.uid);
+      dbUserid.push({email:user.email});
+    });
+  });
+
+  // Listening Login User
+  firebase.auth().onAuthStateChanged(function(user){
+    if(user) {
+      console.log(user);
+      $signInfo.html(user.email+" is login...");
+      $btnSignIn.attr('disabled', 'disabled');
+      $btnSignUp.attr('disabled', 'disabled');
+      $btnSignOut.removeAttr('disabled')
+      // Add a callback that is triggered for each chat message.
+      dbChatRoom.limitToLast(10).on('child_added', function (snapshot) {
+        //GET DATA
+        var data = snapshot.val();
+        var username = data.name || "anonymous";
+        var message = data.text;
+
+        //CREATE ELEMENTS MESSAGE & SANITIZE TEXT
+        var $messageElement = $("<li>");
+        var $nameElement = $("<strong class='example-chat-username'></strong>");
+        $nameElement.text(username);
+        $messageElement.text(message).prepend($nameElement);
+
+        //ADD MESSAGE
+        $messageList.append($messageElement)
+
+        //SCROLL TO BOTTOM OF MESSAGE LIST
+        $messageList[0].scrollTop = $messageList[0].scrollHeight;
+      });//child_added callback
+
+      user.providerData.forEach(function (profile) {
+        console.log("Sign-in provider: "+profile.providerId);
+        console.log("  Provider-specific UID: "+profile.uid);
+        console.log("  Name: "+profile.displayName);
+        console.log("  Email: "+profile.email);
+        console.log("  Photo URL: "+profile.photoURL);
+      });
+    } else {
+      console.log("not logged in");
+    }
+  });
+
+  // SignOut
+  $btnSignOut.click(function(){
+    firebase.auth().signOut();
+    console.log('LogOut');
+    $signInfo.html('No one login...');
+    $btnSignOut.attr('disabled', 'disabled');
+    $btnSignIn.removeAttr('disabled')
+    $btnSignUp.removeAttr('disabled')
+    $message.html('');
+  });
+
+  // LISTEN FOR KEYPRESS EVENT
+  $messageField.keypress(function (e) {
+    if (e.keyCode == 13) {
+      //FIELD VALUES
+      var username = $nameField.val();
+      var message = $messageField.val();
+      console.log(username);
+      console.log(message);
+
+      //SAVE DATA TO FIREBASE AND EMPTY FIELD
+      dbChatRoom.push({name:username, text:message});
+      $messageField.val('');
+    }
+  });
+
+  // Submit
+  $btnSubmit.click(function(){
+    const user = firebase.auth().currentUser;
+    const $userName = $('#userName').val();
+
+    const promise = user.updateProfile({
+      displayName: $userName,
+      photoURL: photoURL
+    });
+    promise.then(function() {
+      console.log("Update successful.");
+    });
+  });
+
+});
