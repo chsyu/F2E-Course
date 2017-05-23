a414d96c14a501cbfa503ae7b7430c44e6d4d08e
Chi-Shan Yu <chsyu@tea.ntue.edu.tw>
2017-05-24 07:09:21 +0800

    wkE - Mixins

--- wkE/gulpfile.js
+++ wkE/gulpfile.js
@@ -1,15 +1,16 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
+mixins = require('postcss-mixins'),
+watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
