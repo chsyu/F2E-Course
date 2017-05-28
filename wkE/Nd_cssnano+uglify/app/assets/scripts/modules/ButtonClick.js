9fa05044ee19b0dc735d4284ab8b89e7f20445aa
Chi-Shan Yu <chsyu@tea.ntue.edu.tw>
2017-05-28 21:34:17 +0800

    wkE - cssnano + uglify

---
+++ wkE/Nd_cssnano+uglify/app/assets/scripts/modules/ButtonClick.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+
+class ButtonClick {
+  constructor() {
+    this.siteButton = $(".site-wrapper__button");
+    this.siteText = $(".site-wrapper__text");
+    this.events();
+  }
+
+  setText() {
+    this.siteText.html('Hello Webpack again ...');
+  };
+
+  events() {
+    this.siteButton.click(() => this.setText());
+  }
+}
+
+export default ButtonClick;
