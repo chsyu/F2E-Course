facde5fca4c8c3fc6f0f7a74f76a7e97853b5214
Yu Chi-Shan <chsyu@tea.ntue.edu.tw>
2017-12-19 15:57:48 +0800

    wkB - GSAP completed!

---
+++ wkB/kb_HelloGSAP/js/script.js
@@ -0,0 +1,6 @@
+// TweenMax.to('.box1', 1, {x:450});
+// TweenMax.to('.box1', 1, {x:450, y:450, 
+//             delay:0.2, 
+//             ease: Elastic.easeOut.config(1, 0.3)
+//             });
+// TweenMax.from('.box1', 1, {x:450});
