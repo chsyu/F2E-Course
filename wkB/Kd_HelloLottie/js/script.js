cdeab6c83efd833e7380abbe8b4da6396ef6b089
chsyu <chsyu@tea.ntue.edu.tw>
2020-05-21 12:36:30 +0800

    lottie file update

--- wkB/Kd_HelloLottie/js/script.js
+++ wkB/Kd_HelloLottie/js/script.js
@@ -8,8 +8,8 @@ $(document).ready(function () {
     loop: true,
     prerender: true,
     autoplay: true,
-    // path: 'json/download-icon.json'
-    path: 'https://labs.nearpod.com/bodymovin/demo/promo/data.json'
+    path: 'json/download-icon.json'
+    // path: 'https://labs.nearpod.com/bodymovin/demo/promo/data.json'
   };
   // set bodymovin
   let openingAnim = bodymovin.loadAnimation(openingAnimData);
