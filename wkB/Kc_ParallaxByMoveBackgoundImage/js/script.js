bb514ea581fbfa74087ceb46678b0f717021f655
chsyu <chsyu@tea.ntue.edu.tw>
2020-11-30 16:22:04 +0800

    Parallax Background Images

---
+++ wkB/Kc_ParallaxByMoveBackgoundImage/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+
+gsap.utils.toArray("section").forEach((section, i) => {
+  section.bg = section.querySelector(".bg"); 
+
+  // Do the parallax effect on each section
+  if (i) {
+
+    gsap.to(section.bg, {
+      backgroundPosition: `50% 250px`,
+      ease: "none",
+      scrollTrigger: {
+        trigger: section,
+        scrub: true
+      }
+    });
+  } 
+  
+  // the first image should be positioned against the top. Use px on the animating part to work with GSAP. 
+  else {
+    gsap.to(section.bg, {
+      backgroundPosition: `50% 250px`,
+      ease: "none",
+      scrollTrigger: {
+        trigger: section,
+        start: "top top", 
+        end: "bottom top",
+        scrub: true
+      }
+    });
+  }
+}); 
+});
