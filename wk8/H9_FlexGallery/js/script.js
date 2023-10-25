e9bce2f7269100fa57da2546d307cf0262067679
Chi-Shan Yu <chisamyu@seed.net.tw>
2023-10-25 08:08:02 +0800

    wk8: add a gallery with rwd and flex example

---
+++ wk8/H9_FlexGallery/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $(".nav__list-item").hover(
+    function () {
+      //When trigger is hovered...
+      $(this).children(".nav__list--submenu").slideDown("fast");
+    },
+    function () {
+      $(this).children(".nav__list--submenu").slideUp("fast");
+    }
+  );
+
+  $(".gallery__item-link").hover(
+    function () {
+      // Fade in overlay
+      $(this).children(".overlay").fadeIn(800);
+    },
+    function () {
+      // Fade out overlay
+      $(this).children(".overlay").fadeOut(500);
+    }
+  );
+});
