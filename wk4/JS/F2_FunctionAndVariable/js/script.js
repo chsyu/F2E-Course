abe3338238546ee240ba4254f14630fb83cd0d66
Chi-Shan Yu <chisamyu@seed.net.tw>
2023-11-29 09:20:36 +0800

    2023: wk3/4 add new dropdown menu style

--- wk4/JS/F2_FunctionAndVariable/js/script.js
+++ wk4/JS/F2_FunctionAndVariable/js/script.js
@@ -1,28 +1,28 @@
-var hello = "I am from global..";
+let hello = "I am from global..";
 alert(hello);
 
 (function(){
-    var hello = "I am from invoking..";
+    let hello = "I am from invoking..";
     alert(hello);
 })();
 
-var age = 18;
+let age = 18;
 
 function sub10(num){
   return num - 10;
 }
-var youngage = sub10(age);
+let youngage = sub10(age);
 alert("youngage = " + youngage);
 
-var add10 = function(num){
+let add10 = function(num){
   return num + 10;
 };
-var oldage = add10(age);
+let oldage = add10(age);
 alert("oldage = " + oldage);
 
 (function(){
   outage = 20;      //Global
-  var height = 170; //Local
+  let height = 170; //Local
   if(outage == 20)
      height = 180;
   alert("height = " + height);
