498aa8e765a785aa422c8f11689a6bae3fda5b79
Chi-Shan Yu <chisamyu@seed.net.tw>
2024-09-26 10:42:21 +0800

    2024: Masonry add imagesLoaded issue

--- wk6/Ga_Masonry/Finished/js/script.js
+++ wk6/Ga_Masonry/Finished/js/script.js
@@ -1,8 +1,16 @@
 
 	$(document).ready(function(){
-		$('.grid').masonry({
-		  itemSelector: '.grid-item',
-		  columnWidth: 200,
-			gutter:20
+		// Masonry
+		imagesLoaded( '.grid', function() {
+			$('.grid').masonry({
+			  itemSelector: '.grid-item',
+			  columnWidth: 200,
+				gutter:20
+			});
 		});
+		// $('.grid').masonry({
+		//   itemSelector: '.grid-item',
+		//   columnWidth: 200,
+		// 	gutter:20
+		// });
 	});
