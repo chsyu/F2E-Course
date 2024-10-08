5649d28210d3df22760102a9b7bd9e40230b536f
Chi-Shan Yu <chisamyu@seed.net.tw>
2024-10-08 08:59:22 +0800

    add swiper cacousel

---
+++ wkA/Je_Swiper/js/script.js
@@ -0,0 +1,35 @@
+const swiper = new Swiper('.swiper-container', {
+   slidesPerView: 1,
+   spaceBetween: 10,
+   loop: true,
+   // autoplay: {
+   //   delay: 500,
+   // //   disableOnInteraction: false,
+   // },
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+   pagination: {
+     el: '.swiper-pagination',
+     clickable: false,
+   },
+   breakpoints: {
+     576: {
+       slidesPerView: 2,
+       spaceBetween: 10,
+     },
+     768: { 
+       slidesPerView: 3,
+       spaceBetween: 10,
+     },
+     992: {
+       slidesPerView: 4,
+       spaceBetween: 10,
+     },
+     1200: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+   },
+ });
