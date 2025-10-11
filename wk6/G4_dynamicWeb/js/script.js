5374692128168869a6ae5cf582c77946f1b859db
Chi-Shan Yu <chisamyu@seed.net.tw>
2025-10-11 13:33:12 +0800

    修改wk6: jquery的dynamic web範例

--- wk6/G4_dynamicWeb/js/script.js
+++ wk6/G4_dynamicWeb/js/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
 
-	const content = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus laborum labore est minima aut, illum facere earum consectetur placeat iste non ducimus voluptate, dolores atque dignissimos fuga veritatis alias itaque harum? Voluptate quasi hic, soluta at possimus porro reprehenderit inventore aliquid perferendis deleniti aut esse. Labore quis corporis similique illum?';
+	const shortContent = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus laborum labore est minima aut, illum facere earum consectetur placeat iste non ducimus ...<span class="more" id="more">more</span>';
+	const fullContent = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus laborum labore est minima aut, illum facere earum consectetur placeat iste non ducimus voluptate, dolores atque dignissimos fuga veritatis alias itaque harum? Voluptate quasi hic, soluta at possimus porro reprehenderit inventore aliquid perferendis deleniti aut esse. Labore quis corporis similique illum?<br><br><span class="collapse" id="collapse">▲</span>';
 
-	$('#more').click(function() {
-		$('#content').html(content);
-	})
+	$('#content').html(shortContent);
+
+	// 點擊 more 展開完整內容
+	$(document).on('click', '#more', function() {
+		$('#content').html(fullContent);
+	});
+
+	// 點擊向上箭頭收回到摘要內容
+	$(document).on('click', '#collapse', function() {
+		$('#content').html(shortContent);
+	});
 
 });
